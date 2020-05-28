d305a56c190d4c3f5b1a56fc300c601ec2f58105
Jun0413 <jzeng004@e.ntu.edu.sg>
2020-05-28 16:58:50 -0700

    upload results

---
+++ mytest/custom.sh
@@ -0,0 +1,12 @@
+echo "fp_1"
+bunzip2 -kc ../traces/fp_1.bz2 | ../src/predictor --custom
+echo "fp_2"
+bunzip2 -kc ../traces/fp_2.bz2 | ../src/predictor --custom
+echo "int_1"
+bunzip2 -kc ../traces/int_1.bz2 | ../src/predictor --custom
+echo "int_2"
+bunzip2 -kc ../traces/int_2.bz2 | ../src/predictor --custom
+echo "mm_1"
+bunzip2 -kc ../traces/mm_1.bz2 | ../src/predictor --custom
+echo "mm_2"
+bunzip2 -kc ../traces/mm_2.bz2 | ../src/predictor --custom
